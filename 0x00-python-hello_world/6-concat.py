2c8b01a869e8337c0bd5b76c5937154d566e5971
Onyi13 <njokuonyi562@gmail.com>
2024-01-02 08:28:46 -0800

    DFSK

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-# This print what's required
-print("{} {}".format(str1, str2))
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
