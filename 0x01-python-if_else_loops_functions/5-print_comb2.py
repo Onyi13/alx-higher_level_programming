60bbe2e17324c37376c88e78f5dad1bfee41d23f
Onyi13 <njokuonyi562@gmail.com>
2024-01-03 03:18:12 -0800

    DKJ

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(str(i).zfill(2), end=", " if i < 99 else "")
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
