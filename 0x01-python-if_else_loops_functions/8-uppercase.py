658e66179c4981d5eaf649a831a74a5d1c924da8
Onyi13 <njokuonyi562@gmail.com>
2024-01-03 12:10:23 -0800

    DKJ

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def uppercase(str):
 for char in str:
-print("{:c}".format(ord(char) - 32)
-        if 97 <= ord(char) <= 122 else char, end="")
+print("{:c}".format(ord(char) - 32) if 97 <= ord(char) <= 122 else char, end="")
 print()
