17b4043279bda68d0b8387d5e3f42bb5a00b75ad
Onyi13 <njokuonyi562@gmail.com>
2024-01-30 10:57:37 -0800

    DSFKJ

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python3
+
 """
 Module containing the Rectangle class.
 """
+
 class Rectangle:
     """
     Defines a rectangle.
     """
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    def get_width(self):
+
+    @property
+    def width(self):
         return self.__width
-    def set_width(self, value):
+
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-    def get_height(self):
+
+    @property
+    def height(self):
         return self.__height
-    def set_height(self, value):
+
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         return 2 * (self.__width + self.__height)
+
