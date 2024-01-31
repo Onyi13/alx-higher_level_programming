c4b6e8f89d0de967d0f640e3f71d3f01335f87b3
Onyi13 <njokuonyi562@gmail.com>
2024-01-31 10:16:31 -0800

    SDKJ

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# This script defines a function copy_list(l) that returns a copy of a list.
+# The input list can contain any type of objects.
+# The function uses list slicing to create a new list.
+def copy_list(l):
+    return l[:]
